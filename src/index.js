fb51356018345a247e75bd8e46956ea6beb71d36
jofer86 <jofer86@gmail.com>
2019-08-23 21:16:01 -0500

    Got response back from API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+async function search() {
+  const cityName = document.querySelector('.city').value;
+  const result = await fetch(
+    `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${cityName}`
+  );
+  const data = await result.json();
+  const cityID = data[0].woeid;
+  const forecast = await fetch(
+    `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${cityID}`
+  );
+  const foreData = await forecast.json();
+  console.log(foreData);
+}
 
+document.querySelector('.button').addEventListener('click', search);
