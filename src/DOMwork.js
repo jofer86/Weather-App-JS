79f619de0c7f570470255c40242abafe3cc49da3
jofer86 <jofer86@gmail.com>
2019-08-26 14:25:07 -0500

    linting??

--- src/DOMwork.js
+++ src/DOMwork.js
@@ -1,27 +1,26 @@
 import search from './apilogic';
 
-let notused = "this is it";
 async function DOMWork() {
-  const reset = document.querySelector('.results');
-  if (reset) {
-    reset.remove();
-  };
+	const reset = document.querySelector('.results');
+	if (reset) {
+		reset.remove();
+	}
 
-  const { city, temp, weatherDescription, weatherIcon } = await search();
-  const appUI = document.querySelector('.appUI');
-  const child = document.createElement('div');  
-  child.className = 'results';
-  appUI.appendChild(child);
-  const info = document.querySelector('.results');  
+	const { city, temp, weatherDescription, weatherIcon } = await search();
+	const appUI = document.querySelector('.appUI');
+	const child = document.createElement('div');  
+	child.className = 'results';
+	appUI.appendChild(child);
+	const info = document.querySelector('.results');  
 
-  info.innerHTML =
+	info.innerHTML =
     `<h1 class="cityName">${city}</h1>
     <h2 class="temperature"> Temperature is ${temp} </h2>
     <h3> The weather today is going to be: ${weatherDescription} </h3>
     <div class="weather" >
         <img src="https://www.weatherbit.io/static/img/icons/${weatherIcon}.png" alt="" width="200"/>
     </div>`
-    ;
+	;
 }
 
 export default DOMWork;
