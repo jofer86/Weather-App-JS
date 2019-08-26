380494e7a51bdbb0fe09b512cbd9cedd1734a0e7
jofer86 <jofer86@gmail.com>
2019-08-26 11:14:11 -0500

    gitminor fixes

--- src/apilogic.js
+++ src/apilogic.js
@@ -1,17 +1,22 @@
 async function search() {
   try {
-    const cityName = document.querySelector('.city').value;    
+    const cityName = document.querySelector('.city').value;
     const result = await fetch(
       `https://api.weatherbit.io/v2.0/current?city=${cityName}&key=c1faea1cee424d32a6d580d4eb1fb86b`
     );
     const data = await result.json();
+    const celsiusTemp = data.data[0].temp;
+    const farenheitTemp = celsiusTemp * (9 / 5) + 32;
+    const weatherIcon = data.data[0].weather.icon;
+    const weatherCode = data.data[0].weather.code;
+    const weatherDescription = data.data[0].weather.description;
 
     const rundown = {
-      celsiusTemp: data.data[0].temp,
-      farenheitTemp: celsiusTemp * (9 / 5) + 32,
-      weatherIcon: data.data[0].weather.icon,
-      weatherCode: data.data[0].weather.code,
-      weatherDescription: data.data[0].weather.description
+      celsiusTemp: celsiusTemp,
+      farenheitTemp: farenheitTemp,
+      weatherIcon: weatherIcon,
+      weatherCode: weatherCode,
+      weatherDescription: weatherDescription
     };
     return rundown;
     
